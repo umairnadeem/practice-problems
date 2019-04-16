46bd8071659a5739812b67e0a3cf97a6055571f8
umairnadeem123 <nadeem.umair123@gmail.com>
2019-04-16 09:28:25 -0700

    Implement RockPaperScissors toy problem to meet all specs

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,33 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+/*
+f(2) = [rr, rp, rs, pr, pp, ps, sr, sp, ss]
+f(3) = [rrr, rrp, ]
+*/
+
+var rockPaperScissors = function (n) {
   // TODO: your solution here
+  var arr = ['R', 'P', 'S'];
+  var output = [];
+  var tempString = '';
+  //I: number of rouds
+  //O: an array of strings containing all permutations of moves
+  //C: none
+  //E: input is 0
+
+    arr.forEach(x => {
+        arr.forEach(y => {
+            arr.forEach(z => {
+                tempString = '';
+                tempString += x;
+                tempString += y;
+                tempString += z;
+                output.push(tempString);
+            });
+        });
+    });
+
+    return output;
 };
 
