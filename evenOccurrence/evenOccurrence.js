aac1bbaa55924e9e80948e313704b8bb06cd99a8
umairnadeem123 <nadeem.umair123@gmail.com>
2019-04-17 09:24:08 -0700

    Implement evenOccurrence function to meet all specs

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -11,5 +11,30 @@
 */
 
 var evenOccurrence = function(arr) {
-  // Your code here.
+  // I: array of numbers
+  // O: the first item that occurs an even number of times
+  // C: none
+  // E: no item occurs an even number of times, there's a tie
+
+  // In an object, create a log of number of occurences of each number
+  // Run through object: the first item that has an even number occurrence, return it
+
+  // Create empty log object
+  var logObj = {};
+  var output = null;
+  // Go through input array and set keys to each array element
+  arr.forEach(element => {
+    // Update key's value for each occurrence
+    logObj[element] = logObj[element] ? logObj[element] + 1: 1;
+  });
+
+  // Go through log in order, if a number occurs an even number of times, return it
+  for (let key in logObj) {
+    if (logObj[key] % 2 === 0) {
+      output = key;
+      break;
+    }
+  }
+
+return output;
 };
