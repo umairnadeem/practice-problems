94c22e7e80371dec78d3ee9738924575498023bc
umairnadeem123 <nadeem.umair123@gmail.com>
2019-04-27 09:50:13 -0700

    Implement more randomness to reduce bias

--- shuffleDeck/shuffleDeck.js
+++ shuffleDeck/shuffleDeck.js
@@ -53,7 +53,7 @@ var shuffleDeck = function(deck) {
   var random = 0;
 
   deck.forEach(() => {
-    random = Math.round(Math.round(Math.random()*(duplicateDeck.length - 1)*100)/100);
+    random = Math.round(Math.round(Math.random()*(duplicateDeck.length - 1)*100000)/100000);
     outputDeck.push(duplicateDeck[random]);
     duplicateDeck.splice(random, 1);
   });
