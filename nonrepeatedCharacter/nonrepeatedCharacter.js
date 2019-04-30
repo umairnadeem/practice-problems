56f2650082f72e45f31dc15ca1a408fa82e40837
umairnadeem123 <nadeem.umair123@gmail.com>
2019-04-30 09:39:47 -0700

    Implement nonrepeatedCharacter toy problem to meet specs

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,32 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+
+  var uniqueArr = [];
+  var duplicateArr = [];
+
+  //go through each character
+    // if it's a character
+      // if not included in uniqueArr or duplicateArr
+        //push to uniqueArr
+      // if included in uniqueArr
+        // remove that value from uniqueArr, push to duplicateArr
+  
+  // return first index of uniqueArr
+
+  for (let char of string) {
+    if (char.toUpperCase() !== char.toLowerCase()) {
+      var uniqueIndex = uniqueArr.indexOf(char);
+      if (uniqueIndex === -1 && duplicateArr.indexOf(char) === -1) {
+        uniqueArr.push(char);
+      } else if (uniqueIndex !== -1) {
+        uniqueArr.splice(uniqueIndex,1);
+        duplicateArr.push(char);
+      }
+    }
+  }
+
+  return uniqueArr[0];
 };
+
+// console.log(firstNonRepeatedCharacter('AACCBDDBc'))
