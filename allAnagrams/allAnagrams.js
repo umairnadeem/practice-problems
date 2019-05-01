3e6654389aeb3c1761fd895f86988686dcec5462
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-01 09:57:55 -0700

    Attempt swapping algo

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,47 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  // n! possibilities
+
+  // I: string
+  // O: array of string permutations
+  // C: none
+  // E: empty string, duplicates
+
+  var arr = [];
+
+  // loop over each char
+    // swap char 0 with char 0
+    // swap char 0,1
+    // swap char 0,2
+    
+    // swap char 1 with char 0
+    // swap char 1 with char 1
+    // swap char 1 with char 2
+
+    // swap char 2 with char 0
+    // swap char 2 with char 1
+    // swap char 2 with char 2
+  // var swapper = (str, x, y) => {
+  //   for (let i in string) {
+  //     var strArr = string.split('');
+  //     [strArr[x], strArr[y]] = [strArr[y], strArr[x]];
+  //     arr.push(strArr.join(''));
+  //   }
+  //   return str;
+  // }
+
+  for (let x in string) {
+    for (let y in string) {
+      var strArr = string.split('');
+      [strArr[x], strArr[y]] = [strArr[y], strArr[x]];
+      arr.push(strArr.join(''));
+    }
+  }
+
+  // return arr;
+
+  return arr.filter((str, i) => arr.indexOf(str) === i);
 };
+
+// console.log(allAnagrams('abc'));
