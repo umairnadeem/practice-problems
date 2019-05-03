76efb3fb36c19104d8809063b403e05e7ea92bc5
umairnadeem123 <nadeem.umair123@gmail.com>
2019-05-03 11:15:24 -0700

    Fix time complexity of algorithm

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -28,27 +28,27 @@ var makeChange = function(total) {
     var denominations = [1,2,5,10,20,50,100,200];
     var permutations = 0;
 
-    function permutator(k) {
+    function permutator(k, arr) {
         var sum = 0;
-        if (k === 1 || k === 0) {
+        if (k === 0) {
             permutations++;
             return;
         }
         
-        denominations.forEach(n => {
+        arr.forEach((n, i) => {
             if (n <= (k - sum)) {
                 sum += n;
-                permutator(k - sum);
+                permutator(k - sum, arr.slice(i));
                 sum = 0;
             }
         });
     }
 
-    permutator(total);
+    permutator(total, denominations);
 
     return permutations;
 };
 
-// console.log(makeChange(10));
+// console.log(makeChange(200));
 
 
