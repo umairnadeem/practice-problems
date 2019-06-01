312d0c2fe9a2982cef5ea6f5c6ca982b2b357a60
umairnadeem123 <nadeem.umair123@gmail.com>
2019-06-01 09:51:47 -0700

    Implement linkedListIntersection

--- linkedListIntersection/linkedListIntersection.js
+++ linkedListIntersection/linkedListIntersection.js
@@ -18,6 +18,7 @@
 *             ↗
 *        X → Y
 * 
+*     
 * Given the following two linked lists list1 and list2, 
 * linkedListIntersection(list1,list2) should return NULL 
 * as there is no point of intersection.
@@ -39,5 +40,60 @@ function Node (val) {
 
 function linkedListIntersection(list1, list2) {
   // TODO: Implement this function!
+
+  // Traverse longer list first
+  // Start traversing both nodes in parallel 
+    // If common node found, set to true
+    // else bool is false
+
+  let result;
+
+  const length = (list) => {
+    let count = 0;
+    while (list) {
+      list.next;
+      list = list.next;
+      count++;
+    }
+    // console.log(count)
+    return count;
+  }
+
+  const length1 = length(list1);
+  const length2 = length(list2);
+  const findIntersection = (long, short, diff) => {
+    let output = null;
+    while (diff) {
+      long = long.next;
+      diff--;
+    }
+    while (long && short) {
+      if (long === short) {
+        output = long;
+        break;
+      }
+      long = long.next;
+      short = short.next;
+    }
+    return output;
+  }
+
+  if (length1 > length2) {
+    result = findIntersection(list1, list2, length1 - length2);
+  } else {
+    result = findIntersection(list2, list1, length2 - length1);
+  }
+  return result;
 }
 
+// let a = Node('a')
+// let b = Node('b')
+// let c = Node('c')
+
+// const list1 = a;
+// list1.next = b;
+// list1.next.next = c;
+
+// const list2 = Node('a');
+
+// console.log(linkedListIntersection(list1, list2))
