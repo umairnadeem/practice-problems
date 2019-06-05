0543f2dd0330d63c89e3f7af162b66aefd51e50f
umairnadeem123 <nadeem.umair123@gmail.com>
2019-06-05 09:48:59 -0700

    Make solver function

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -21,6 +21,36 @@ Example input:
 
 
 function sudokuChecker(board) {
-  // Your code here.
+  // Check rows
+  const rows = board.split('\n');
+  const rowsBool = rows.reduce((accum, row) => accum && checkNumbers(row), true);
+
+  // Check cols
+  const cols = rows.map((row, i) => rows.map(col => col[i]).join(''));
+  const colsBool = cols.reduce((accum, col) => accum && checkNumbers(col), true);
+
+  // Check matrices
+  let matrices = [];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      let str = '';
+      for (let x = j * 3; x < j * 3 + 3; x++) {
+        str += rows[x].slice(i * 3, i * 3 + 3);
+      }
+      matrices.push(str);
+    }
+  }
+  const matricesBool = matrices.reduce((accum, matrix) => accum && checkNumbers(matrix), true);
   
+  return rowsBool && colsBool && matricesBool ? 'solved' : 'invalid';
 }
+
+function checkNumbers(string) {
+  let sum = 0;
+  for (let char of string) {
+    sum += +char;
+  }
+  return sum === 45 && string.length === 9 && string.split('').filter((char, index, array) => array.indexOf(char) === index).length === 9;
+}
+
+// console.log(sudokuChecker(`735814296\n896275314\n214963857\n589427163\n362189745\n471356982\n923541678\n648792531\n157638429`));
