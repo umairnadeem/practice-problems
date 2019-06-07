d801a8a0e63b84a28bdb0d761732a1f7efb50c56
umairnadeem123 <nadeem.umair123@gmail.com>
2019-06-07 09:50:11 -0700

    Implement integerReverse recursively

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -9,6 +9,30 @@
 *
 */
 
-function reverseInteger(number){
-  // TODO: Implement this function!
+function reverseInteger(number){  
+  // find decimal place: 10 ^ (Math floor (log base 10 (n))) -> 1000 -> store in separate variable
+  // divide number by decimal place and math.floor * decimal place -> 3000
+    // if number not single-digit, subtract from total sum and repeat process
+    // if number is single digit, multiply by separate variable and add to output number
+    // divide separate number by 10
+  
+  let decimalMain = Math.pow(10, Math.floor(Math.log10(number))); // 1000
+  let output = 0;
+
+  function recursiveReverse(n) {
+    let decimalRecursive = Math.pow(10, Math.floor(Math.log10(n))); // 1000
+    let extract = Math.floor(n / decimalRecursive) * decimalRecursive; // 3000
+    if (decimalRecursive !== 1) {
+      recursiveReverse(n - extract);
+    }
+    output += Math.floor(n / decimalRecursive) * decimalMain;
+    decimalMain /= 10;
+  }
+
+  recursiveReverse(number);
+
+  return output;
 }
+
+
+// console.log(reverseInteger(5423));
