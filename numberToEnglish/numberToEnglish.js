eac4b85adbcdf9dde7bbedda74702a351314dec2
umairnadeem123 <nadeem.umair123@gmail.com>
2019-06-12 10:49:27 -0700

    Complete requirements

--- numberToEnglish/numberToEnglish.js
+++ numberToEnglish/numberToEnglish.js
@@ -65,30 +65,36 @@ Number.prototype.toEnglish = function () {
     return `${numbersToWords[this - (this % 10)]} ${(this % 10).toEnglish()}`;
   }
 
-  // 100 - 999
-  if (this >= 100) {
-    let output = ``;
+  // 100 - 9999
+  if (this < 10000) {
+    let placement;
+    for (let place in numbersToPlace) {
+      if (Math.floor(this / place) < 10) {
+        placement = place;
+        break;
+      }
+    }
+    return `${Math.floor(this / placement).toEnglish()} ${numbersToPlace[placement]} ${(this % placement).toEnglish()}`;
+  } else {
     let n = this;
+    let i = 0;
+    const thousand = 1e3;
 
-    let place = 100;
-    while (Math.floor(this / place) >= 10) {
-      place *= 10;
+    while (n >= thousand) {
+      n /= thousand;
+      i++;
     }
 
-    while (n) {
-      output += `${Math.floor(n / place).toEnglish()} `;
-      n -= Math.floor(n / place);
-    } 
-  } 
+    return `${Math.floor(n).toEnglish()} ${numbersToPlace[thousand ** i]} ${(this % (thousand ** i)).toEnglish()}`;
+  }
+
 };
 
-// 999
-// 1000
-
-console.log((150043).toEnglish());
+// console.log((9148237412).toEnglish());
 
 // 0 - 20 -> direct translations
 // 21 - 99 ->  direct translation of 1st place + direct translation of 2nd place
   // direct translation(n - (n % 10)) + direct translation (n % 10)
 // 100 - 999 ->  direct translation (Math.floor(n / 100)) + hundred + recursive(n % 100)
 // 1000 - 9999 -> direct translation (Math.floor(n / 1000)) + thousand + recursive(n % 1000)
+// 10,000 - 99,999 -> 
