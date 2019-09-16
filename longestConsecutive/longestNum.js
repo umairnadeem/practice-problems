72e014223e7909bffbfe9bbd9ae1d2e707d5595a
umairnadeem123 <nadeem.umair123@gmail.com>
2019-09-16 14:10:15 -0700

    Complete longestConsecutive in linear time and linear space

--- longestConsecutive/longestNum.js
+++ longestConsecutive/longestNum.js
@@ -1,29 +1,70 @@
-var longestConsecutive = function(nums) {
-    const memo = {};
-    let max = 0;
-    nums.forEach((num) => {
-      if (memo[num] === undefined) {
-        if (memo[num + 1]) {
-          memo[num + 1] = num;
-        } else memo[num] = num;
-        if (memo[num - 1]) {
-          memo[num] = memo[num - 1];
-          delete memo[num - 1];
-        }
-      }
-    });
-    Object.keys(memo).forEach((key) => {
-      if (memo[memo[key]] !== undefined) {
-        const store = memo[key];
-        memo[key] = memo[memo[key]];
-        delete memo[store];
-      }
-    });
-    return memo;
-};
+/*
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 
+Your algorithm should run in O(n) complexity.
+
+Example:
+
+Input: [100, 4, 200, 1, 3, 2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+*/
+
+var longestConsecutive = function(nums) {
+  if (!nums.length) return 0;
+  const memo = {};
+  const temp = {};
+  let maxRange = 0;
+  const newNums = [];
+  nums.forEach((num) => {
+    if (!temp[num]) newNums.push(num);
+    temp[num] = true;
+  });
+  newNums.forEach((num) => {
+    if (memo[num + 1] !== undefined && memo[num - 1] !== undefined) {
+      const storedLower = memo[num - 1];
+      const storedUpper = memo[num + 1];
+      delete memo[num + 1];
+      delete memo[num - 1];
+      if (memo[storedLower] !== undefined) delete memo[storedLower];
+      if (memo[storedUpper] !== undefined) delete memo[storedUpper];
+      memo[storedLower] = storedUpper;
+      memo[storedUpper] = storedLower;
+    } else if (memo[num + 1] !== undefined) {
+      const storedVal = memo[num + 1];
+      delete memo[num + 1];
+      memo[num] = storedVal;
+      memo[storedVal] = num;
+    } else if (memo[num - 1] !== undefined) {
+      const storedVal = memo[num - 1];
+      delete memo[num - 1];
+      memo[num] = storedVal;
+      memo[storedVal] = num;
+    } else {
+      memo[num] = num;
+    }
+  });
+  Object.keys(memo).forEach((key) => {
+    if (memo[key] && memo[key] !== parseInt(key)) delete memo[memo[key]];
+  });
+  Object.keys(memo).forEach((key) => {
+    const range = Math.abs(parseInt(key) - memo[key]);
+    maxRange = Math.max(maxRange, range);
+  });
+  return maxRange + 1;
+};
 console.log(
   longestConsecutive(
-    [9,4,10,2,1,3,4,-1,0,-2,-3,-4,-5,4]
+    [-1,0,-2,-3,-4,-5,4]
+  ),
+  longestConsecutive(
+    [4,3,2,5,0,1]
+  ),
+  longestConsecutive(
+    [100, 4, 200, 1, 3, 2]
+  ),
+  longestConsecutive(
+    [4,2,2,-4,0,-2,4,-3,-4,-4,-5,1,4,-9,5,0,6,-8,-1,-3,6,5,-8,-1,-5,-1,2,-9,1]
   )
 )
